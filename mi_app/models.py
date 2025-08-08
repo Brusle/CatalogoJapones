92e0cfab1186926e7b85681f2ae4beab74c3d772
Mauricio Glz <gu23ia0008@globalunivesity.edu.mx>
2025-08-07 20:21:21 -0600

    Cambios de clases

---
+++ mi_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    correo = models.EmailField(unique=True)
+    telefono = models.CharField(max_length=20,)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
---
+++ mi_app/views.py
@@ -0,0 +1,47 @@
+from django.http import JsonResponse, HttpResponseBadRequest
+from django.views import View
+import json
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+
+from .models import Cliente
+
+#Create your views here.
+class ClienteView(View):
+    @method_decorator(csrf_exempt, name='dispatch')
+    def get (self, request):
+        Clientes = Cliente.objects.all().values()
+        return JsonResponse(list(Clientes), safe=False)
+    
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            cliente = cliente.objects.create(
+                nombre=data['nombre'],
+                apellido=data['apellido'],
+                correo=data['correo'],
+                telefono=data['telefono'])
+            return JsonResponse({"mensaje": "Cliente creado", "id": cliente.nombre})
+        except:
+            return HttpResponseBadRequest({"Error": "Formato Invalido"})
+            
+"""
+    def get(self, request):
+        data = {
+            "respuesta": "Hola Mundo"
+        }
+        return JsonResponse(data)
+    
+    def post(self, request):
+        try:
+            body = json.loads(request.body)
+        except:
+            return HttpResponseBadRequest("Formato Invalido")
+        
+        data = {
+            "status": "ok",
+        }
+
+        return JsonResponse(data)
+        """
