617960410f68f3e9e7f3d58954337eedb22a7a7c
Mauricio Glz <gu23ia0008@globalunivesity.edu.mx>
2025-08-12 16:53:36 -0600

    Agregamos la nueva URLs

--- CatalogoJapones/urls.py
+++ CatalogoJapones/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from mi_app.views import WelcomeView, SearchSelectionView, SearchView
+from mi_app.views import WelcomeView, SearchSelectionView, SearchView, CharacterListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', WelcomeView.as_view(), name='welcome'),
     path('search/', SearchSelectionView.as_view(), name='search_selection'),
     path('search/results/', SearchView.as_view(), name='search_results'),
+    path('characters/', CharacterListView.as_view(), name='character_list'),
 ]
