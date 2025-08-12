654a870fe196a7aebb6e4cee47a3b39163823b5e
Mauricio Glz <gu23ia0008@globalunivesity.edu.mx>
2025-08-12 16:52:24 -0600

    Cargamos una nueva clase en Views

--- mi_app/views.py
+++ mi_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Cliente
 from django.views import View
-from vector_db import buscar_personajes
+from vector_db import buscar_personajes, cargar_datos_verificados
+
 
 class WelcomeView(View):
     def get(self, request):
@@ -22,3 +23,15 @@ class SearchView(View):
             resultados = []
             
         return render(request, 'search_results.html', {'resultados': resultados, 'query': query})
+    
+class CharacterListView(View):
+    def get(self, request):
+        # Cargamos los datos de los personajes
+        df_personajes = cargar_datos_verificados()
+        # Convertimos el DataFrame a una lista de diccionarios para usarla en la plantilla
+        lista_personajes = df_personajes.to_dict('records')
+        
+        context = {
+            'personajes': lista_personajes
+        }
+        return render(request, 'character_list.html', context)
